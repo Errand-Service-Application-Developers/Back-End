28d0d0b8804875d20af4c5e9185327d0a122bf1f
Pinto Tetteh Aaron <aaronpinto111@gmail.com>
2023-07-13 19:37:22 +0000

    preparing for deployment

--- ErrandGo/settings/prod.py
+++ ErrandGo/settings/prod.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from .common import *
 
 DEBUG = False
@@ -6,4 +7,11 @@ DEBUG = False
 SECRET_KEY = os.environ['SECRET_KEY']
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['errandgo-prod-29e25f29f785.herokuapp.com']
+
+
+
+DATABASES = {
+     'default': dj_database_url.config()
+      
+}
